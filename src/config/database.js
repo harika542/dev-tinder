20da80c84d1a74388201bb1189d738f7321d29a3
harika542 <harikagidituri@gmail.com>
2026-07-14 20:06:57 +0530

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
+//"mongodb+srv://harikagidituri_db_user:harika542@cluster0.1tpfv7.mongodb.net/devTinde
 const connect = async () => {
+  try {
     await mongoose.connect(
-      "mongodb+srv://harikagidituri_db_user:Harii542@cluster0.1tptfv7.mongodb.net/?appName=Cluster0"
-    )};
-module.exports=connect;
-//"mongodb+srv://harikagidituri_db_user:harika542@cluster0.1tpfv7.mongodb.net/devTinder"
+      "mongodb+srv://harikagidituri_db_user:Harii542@cluster0.1tptfv7.mongodb.net/devTinder?appName=Cluster0"
+    );
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("DATABASE ERROR:");
+    console.log(err);
+    throw err;
+  }
+};
 
+module.exports = connect;
