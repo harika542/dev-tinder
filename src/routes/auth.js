43a2ae17e2ab1fa6a66c207372a7a19d5245e2a7
harika542 <harikagidituri@gmail.com>
2026-07-22 16:45:38 +0530

    ok

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,7 +60,9 @@ authRouter.post("/login",async (req,res)=>{
 });
 authRouter.post("/logout",async(req,res)=>{
   res.cookie("token",null,{expires:
-    new Date(Date.now())
+    new Date(Date.now(0)),
+    httpOnly: true,
+    sameSite: "strict",
   });
   res.send("Logout suceessfull!!");
 })
